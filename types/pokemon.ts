bf2a8f55aa70a5262a833e9d33a64f345d9dcd82
dithanrchy <dithanurcahya55@gmail.com>
2025-05-10 14:38:21 +0700

    feat: create pokemon data types

---
+++ types/pokemon.ts
@@ -0,0 +1,50 @@
+// types/pokemon.ts
+export type PokemonType = {
+  pokemon_v2_type: { name: string }
+}
+
+export type PokemonStat = {
+  base_stat: number
+  pokemon_v2_stat: { name: string }
+}
+
+export type PokemonSprites = {
+  sprites: string
+}
+
+export type PokemonSpecies = {
+  gender_rate: number
+  capture_rate: number
+  hatch_counter: number
+  pokemon_v2_growthrate: { name: string }
+}
+
+export type PokemonAbility = {
+  pokemon_v2_ability: { name: string }
+}
+
+export type RawPokemon = {
+  id: number
+  name: string
+  height: number
+  weight: number
+  pokemon_v2_pokemonabilities: PokemonAbility[]
+  pokemon_v2_pokemontypes: PokemonType[]
+  pokemon_v2_pokemonstats: PokemonStat[]
+  pokemon_v2_pokemonsprites: PokemonSprites[]
+  pokemon_v2_pokemonspecy: PokemonSpecies
+}
+
+export type ParsedPokemon = {
+  id: number
+  name: string
+  height: number
+  imageUrl: string
+  types: string[]
+  stats: { name: string; value: number }[]
+  growthRate?: string
+  genderRate?: number
+  hatchSteps?: number
+  effortValues?: string
+  description?: string
+}
